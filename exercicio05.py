27b6463ddc9d874505a5766b0c0e846c6561b7f0
Guilherme Lima <enriqueguilherme1209@gmail.com>
2025-04-01 11:04:35 -0300

    calculo media aprovado e reprovado

---
+++ exercicio05.py
@@ -0,0 +1,11 @@
+print("Bem vindo ao calculo de média!")
+
+n1 = float(input("Digite a nota 1: "))
+n2 = float(input("Digite a nota 2: "))
+n3 = float(input("Digite a nota 3: "))
+media = (n1 + n2 + n3) / 3
+
+if media >= 7:
+    print(f"Aprovado! Sua média foi {media}")
+else:
+    print(f"Reprovado!Sua média foi {media}")
