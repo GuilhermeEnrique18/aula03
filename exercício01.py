71c1624e256e29b33608e0e1c11ab36e01fc32c8
Guilherme Lima <enriqueguilherme1209@gmail.com>
2025-04-01 08:53:05 -0300

    primeira task

---
+++ exercício01.py
@@ -0,0 +1,5 @@
+salario = float(input("Informe seu salário: "))
+nome = input("Informe seu nome: ")
+idade = int(input("Informe sua idade: "))
+
+print(f"Seu nome é: {nome} \n sua idade é: {idade} \n seu salário é: {salario}")
