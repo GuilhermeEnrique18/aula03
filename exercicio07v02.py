6f5164e5d4f4059c46205c8d6ab0fd0941da6669
Guilherme Lima <enriqueguilherme1209@gmail.com>
2025-04-03 09:13:44 -0300

    edicao v2 com tratamento de erro para letra minuscula e comentarios

--- exercicio07v02.py
+++ exercicio07v02.py
@@ -1,13 +1,15 @@
+#Programa para calcular valor de pagamento de acordo com tipo do combustível e quantidade de litros
 tipo = input("Digite o tipo abastecido 'E' para Etanol e 'G': ")
-
 qlitros = float(input("Informe a quantidade de litros desejada: "))
-
+#Declaração das váriaveis gasolina e etanol
 vGaso = 5.80
 vEta = 4.90
-
-if tipo == "G":
+#Condição para entrar na gasolina ou etanol
+if tipo == "G" or tipo == "g":
     valor = vGaso * qlitros
-else:
+elif tipo == "E" or tipo == "e":
     valor = vEta * qlitros
+else:
+    print("Tipo inválido, informe E para Etanol ou G para gasolina")
 
 print(f"{valor:.2f}")
