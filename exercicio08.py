42ab12ae3a7d4ddbcc2c9877f73b0c599a92fcc2
Guilherme Lima <enriqueguilherme1209@gmail.com>
2025-04-03 09:31:32 -0300

    Codigo par ou impar

---
+++ exercicio08.py
@@ -0,0 +1,5 @@
+numerUsuario = int(input("Digite um numéro para saber se é par ou impar"))
+if numerUsuario % 2 == 0:
+    print(f"O número {numerUsuario} é par!")
+else:
+    print(f"O número {numerUsuario} é ímpar!")
