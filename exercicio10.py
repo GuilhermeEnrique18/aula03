0a8f03e549dfd476791e0ab24051e6741384a6fb
Guilherme Lima <enriqueguilherme1209@gmail.com>
2025-04-10 08:48:16 -0300

    criacao de for para imprimir tabuada

--- exercicio10.py
+++ exercicio10.py
@@ -2,4 +2,4 @@ soma = 0
 for i in range (10):
     numero = int(input(f"informe o numero da posição: {i} "))
     soma = soma + numero
-print(soma)   
+print(soma)
---
+++ exercicio11.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um número para saber a tabuada"))
+for i in range (1, 11):
+    mult = numero * i
+    print(f" {numero} x {i} é: {mult}")
